77a32e95b905fb56001607f65e6c342663d37509
jescriba <escribirajoshua@gmail.com>
2016-10-15 17:03:19 -0700

    Adds release date and rating to details

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var infoView: UIView!
     @IBOutlet weak var posterView: UIImageView!
@@ -22,14 +23,30 @@ class DetailViewController: UIViewController {
         view.backgroundColor = UIColor.black
         
         let overview = movie["overview"]
-        titleLabel.text = "⌃"
-        titleLabel.adjustsFontSizeToFitWidth = true
+        let rating = movie["vote_average"] as? Double
+        let releaseDate = movie["release_date"] as? String
         overviewLabel.text = overview as? String
         overviewLabel.sizeToFit()
         // In events of overflowing text still
         overviewLabel.adjustsFontSizeToFitWidth = true
-        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: infoView.frame.maxY)
+        if let rating = rating {
+            ratingLabel.text = NSString(format: "❤︎ %.1f%%", rating / 10.0 * 100) as String
+            ratingLabel.sizeToFit()
+        } else {
+            ratingLabel.text = ""
+        }
+        if let releaseDate = releaseDate {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let date = dateFormatter.date(from: releaseDate)
+            dateFormatter.dateStyle = .medium
+            let formattedDate = dateFormatter.string(from: date!)
+            releaseDateLabel.text = formattedDate
+        } else {
+            releaseDateLabel.text = ""
+        }
         
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: infoView.frame.maxY)
         if let posterPath = movie["poster_path"] as? String {
             let baseURL = "https://image.tmdb.org/t/p/w500"
             let imageURL = URL(string: baseURL + posterPath)
